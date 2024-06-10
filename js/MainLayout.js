0c5df6e6c0a20f3ff554ff3074f2f7d5239d4496
Prosenjit Sarkar <prosenjit@digitalwoods.net>
2024-06-10 15:20:28 +0530

    main layout remove class component

--- js/MainLayout.js
+++ js/MainLayout.js
@@ -1,40 +1,38 @@
-class MainLayout extends React.Component {
-  render() {
-    return (
-      <div className="bg-light dark:bg-slate-900">
-        <ul role="nav">
-          <li>
-            <Link className="text-dark dark:text-light" to="/home">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link className="text-dark dark:text-light" to="/recoil-js">
-              Recoil js
-            </Link>
-          </li>
-          <li>
-            <Link className="text-dark dark:text-light" to="/tanstack-query">
-              Tnstack Query
-            </Link>
-          </li>
-          <li>
-            <Link className="text-dark dark:text-light" to="/react-form">
-              React Form
-            </Link>
-          </li>
-          <li>
-            <ThemeSwitcher />
-          </li>
-        </ul>
+const MainLayout = () => {
+  return (
+    <div className="bg-light dark:bg-slate-900">
+      <ul role="nav">
+        <li>
+          <Link className="text-dark dark:text-light" to="/home">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="text-dark dark:text-light" to="/recoil-js">
+            Recoil js
+          </Link>
+        </li>
+        <li>
+          <Link className="text-dark dark:text-light" to="/tanstack-query">
+            Tnstack Query
+          </Link>
+        </li>
+        <li>
+          <Link className="text-dark dark:text-light" to="/react-form">
+            React Form
+          </Link>
+        </li>
+        <li>
+          <ThemeSwitcher />
+        </li>
+      </ul>
 
-        <div>
-          <Route path="/home" component={Home} />
-          <Route path="/recoil-js" component={Recoiljs} />
-          <Route path="/tanstack-query" component={TnstackQuery} />
-          <Route path="/react-form" component={ReactForm} />
-        </div>
+      <div>
+        <Route path="/home" component={Home} />
+        <Route path="/recoil-js" component={Recoiljs} />
+        <Route path="/tanstack-query" component={TnstackQuery} />
+        <Route path="/react-form" component={ReactForm} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
