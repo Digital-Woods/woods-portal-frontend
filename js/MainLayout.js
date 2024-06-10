075d3c316f80c1c8284fcbc41dfd77ac6d57c8bc
Prosenjit Sarkar <prosenjit@digitalwoods.net>
2024-06-10 18:03:17 +0530

    change route link

--- js/MainLayout.js
+++ js/MainLayout.js
@@ -77,13 +77,13 @@ const MainLayout = () => {
               <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
                 <ThemeSwitcher />
                 <Link
-                  to="/sign-in"
+                  to="/react-form"
                   className="whitespace-nowrap text-base text-gray-500 hover:text-gray-900"
                 >
                   Sign in
                 </Link>
                 <Link
-                  to="/sign-up"
+                  to="/react-form"
                   className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                 >
                   Sign up
