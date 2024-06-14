482c63a7bf102c63f6a2be9856cdc34d5d033eb8
Prosenjit Sarkar <prosenjit@digitalwoods.net>
2024-06-14 13:16:36 +0530

    component add ref

--- js/components/UI/Button.js
+++ js/components/UI/Button.js
@@ -1,6 +1,9 @@
 // const { Redirect } = ReactRouterDOM;
 
-const Button = ({ type = "button", onClick, ...props, isLoading, href }) => {
+const Button = (
+  { type = "button", onClick, ...props, isLoading, href },
+  ref
+) => {
   // const redirect = Redirect();
 
   const onClickHandel = () => {
@@ -13,6 +16,7 @@ const Button = ({ type = "button", onClick, ...props, isLoading, href }) => {
   };
   return (
     <button
+      ref={ref}
       type={type}
       onClick={onClick}
       className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
--- js/components/UI/Card.js
+++ js/components/UI/Card.js
@@ -1,9 +1,13 @@
 const classNames = window.classNames;
 
-const Card = ({ className, ...props }) => {
+const Card = ({ className, ...props }, ref) => {
   return (
     <div
-      className={classNames('max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700', className)}
+      ref={ref}
+      className={classNames(
+        "max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700",
+        className
+      )}
       {...props}
     />
   );
