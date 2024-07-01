9665f4e128b0bcdab2090a56880e983429e21f92
Prosenjit Sarkar <prosenjit@digitalwoods.net>
2024-07-01 12:38:12 +0530

    add dynamic route

--- js/MainLayout.js
+++ js/MainLayout.js
@@ -1,16 +1,57 @@
 const MainLayout = ({ children }) => {
+  const routes = [
+    {
+      path: "/sites",
+      component: Sites,
+      title: "Sites",
+      description: "Manage your sites",
+    },
+    {
+      path: "/recoil-js",
+      component: Recoiljs,
+      title: "Recoil JS",
+      description: "Learn about Recoil JS",
+    },
+    {
+      path: "/tanstack-query",
+      component: TnstackQuery,
+      title: "TanStack Query",
+      description: "Learn about TanStack Query",
+    },
+    {
+      path: "/login",
+      component: Login,
+      title: "Login",
+      description: "Login to your account",
+    },
+    {
+      path: "/register",
+      component: Register,
+      title: "Register",
+      description: "Register a new account",
+    },
+    {
+      path: "/details",
+      component: Details,
+      title: "Details",
+      description: "View details",
+    },
+  ];
   return (
     <div className="flex">
       <SideLayout />
       <div className="w-3/4 bg-light dark:bg-light p-4">
         <HeaderLayout />
         <div className="px-4 py-6">
-          <Route path="/sites" component={Sites} />
-          <Route path="/recoil-js" component={Recoiljs} />
-          <Route path="/tanstack-query" component={TnstackQuery} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/details" component={Details} />
+          {routes.map(({ path, component: Component, title, description }) => (
+            <Route
+              key={path}
+              path={path}
+              render={(props) => (
+                <Component {...props} title={title} description={description} />
+              )}
+            />
+          ))}
         </div>
       </div>
     </div>
