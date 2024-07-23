6cf801c1afc361dc7809420b9dc6e99eeffdf71d
Prosenjit Sarkar <prosenjit@digitalwoods.net>
2024-07-23 17:29:13 +0530

    details page fix loader

--- js/pages/Details.js
+++ js/pages/Details.js
@@ -1,34 +1,9 @@
 const { useState } = React;
 const { useQuery } = ReactQuery;
 
-const Loader = () => (
-  <div role="status" className="flex items-center justify-center p-4">
-    <svg
-      aria-hidden="true"
-      className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
-      viewBox="0 0 100 101"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-        fill="currentColor"
-      />
-      <path
-        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-        fill="currentFill"
-      />
-    </svg>
-    <span className="sr-only">Loading...</span>
-  </div>
-);
-
 const formatKey = (key) => {
-  return key
-    .replace(/_/g, ' ')
-    .replace(/\b\w/g, (char) => char.toUpperCase());
+  return key.replace(/_/g, " ").replace(/\b\w/g, (char) => char.toUpperCase());
 };
-
 
 const Details = ({ path, id }) => {
   const [activeTab, setActiveTab] = useState("overview");
@@ -69,7 +44,10 @@ const Details = ({ path, id }) => {
   const priorityKeys = ["name", "description", "email", "city"];
 
   const filteredAndSortedEntries = (obj) => {
-    const entries = Object.entries(obj).filter(([key, value]) => key !== "id" && key !== "archived" && typeof value !== "object");
+    const entries = Object.entries(obj).filter(
+      ([key, value]) =>
+        key !== "id" && key !== "archived" && typeof value !== "object"
+    );
 
     entries.sort(([keyA], [keyB]) => {
       const isNameA = keyA.toLowerCase().includes("name");
@@ -84,15 +62,27 @@ const Details = ({ path, id }) => {
       if (indexA === -1 && indexB !== -1) return 1;
 
       if (indexA === -1 && indexB === -1) {
-        if (["createdAt", "updatedAt"].includes(keyA) && !["createdAt", "updatedAt"].includes(keyB)) return 1;
-        if (["createdAt", "updatedAt"].includes(keyB) && !["createdAt", "updatedAt"].includes(keyA)) return -1;
+        if (
+          ["createdAt", "updatedAt"].includes(keyA) &&
+          !["createdAt", "updatedAt"].includes(keyB)
+        )
+          return 1;
+        if (
+          ["createdAt", "updatedAt"].includes(keyB) &&
+          !["createdAt", "updatedAt"].includes(keyA)
+        )
+          return -1;
         return 0;
       }
       return indexA - indexB;
     });
 
-    const additionalEntries = entries.filter(([key]) => ["createdAt", "updatedAt"].includes(key));
-    const sortedEntries = entries.filter(([key]) => !["createdAt", "updatedAt"].includes(key));
+    const additionalEntries = entries.filter(([key]) =>
+      ["createdAt", "updatedAt"].includes(key)
+    );
+    const sortedEntries = entries.filter(
+      ([key]) => !["createdAt", "updatedAt"].includes(key)
+    );
     return [...sortedEntries, ...additionalEntries];
   };
 
@@ -133,24 +123,29 @@ const Details = ({ path, id }) => {
           </Tabs>
         </div>
 
-        {activeTab === "overview" ? (
+        {isLoading && !item ? (
+          <div className="loader-line"></div>
+        ) : (
           <div>
-            {isLoading && !item ? (
-              <Loader />
-            ) : (
-              <div className="py-3 dark:bg-gray-900 border border-2 rounded-md my-10 dark:text-white">
-                {item &&
-                  filteredAndSortedEntries(item).map(([key, value]) => (
-                    <div key={key} className="py-4 px-3 flex gap-x-5 border-b">
-                      <div className="font-semibold">{formatKey(key)}:</div>
-                      <div>{renderCellContent(key, value)}</div>
-                    </div>
-                  ))}
+            {activeTab === "overview" ? (
+              <div>
+                <div className="py-3 dark:bg-gray-900 border border-2 rounded-md my-10 dark:text-white">
+                  {item &&
+                    filteredAndSortedEntries(item).map(([key, value]) => (
+                      <div
+                        key={key}
+                        className="py-4 px-3 flex gap-x-5 border-b"
+                      >
+                        <div className="font-semibold">{formatKey(key)}:</div>
+                        <div>{renderCellContent(key, value)}</div>
+                      </div>
+                    ))}
+                </div>
               </div>
+            ) : (
+              <div>2</div>
             )}
           </div>
-        ) : (
-          <div>2</div>
         )}
       </div>
 
@@ -220,7 +215,10 @@ const Details = ({ path, id }) => {
                                           key={itemKey}
                                           className="px-4 py-2 text-gray-900 dark:text-gray-100"
                                         >
-                                          {renderCellContent(itemKey, itemValue)}
+                                          {renderCellContent(
+                                            itemKey,
+                                            itemValue
+                                          )}
                                         </TableCell>
                                       ))}
                                   </TableRow>
