7c7cbfb77ac6a5f80b7fe1cf1cd70904460bf777
Ankan <ankanganguly24@gmail.com>
2024-07-26 16:41:46 +0530

    design fixes

--- js/components/SiteLayout/SideLayout.js
+++ js/components/SiteLayout/SideLayout.js
@@ -25,7 +25,7 @@ const SideLayout = () => {
 
   return (
     <div
-      className={`min-h-screen px-6 pt-6 pb-8 transition-width bg-sidelayoutColor duration-300  dark:bg-dark-200 hidden lg:block`}
+      className={`min-h-full px-6 pt-6 pb-8 transition-width bg-sidelayoutColor duration-300  dark:bg-dark-200 hidden lg:block`}
     >
       <div>
         <div className="flex justify-between items-center mb-8 h-[50px]">
@@ -117,7 +117,7 @@ const SideLayout = () => {
               }`}
             >
               <div>
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" className="fill-white"><path d="M160-200v-80h80v-280q0-83 50-147.5T420-792v-28q0-25 17.5-42.5T480-880q25 0 42.5 17.5T540-820v28q80 20 130 84.5T720-560v280h80v80H160Zm320-300Zm0 420q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80ZM320-280h320v-280q0-66-47-113t-113-47q-66 0-113 47t-47 113v280Z"/></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" className="fill-white"><path d="M160-200v-80h80v-280q0-83 50-147.5T420-792v-28q0-25 17.5-42.5T480-880q25 0 42.5 17.5T540-820v28q80 20 130 84.5T720-560v280h80v80H160Zm320-300Zm0 420q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80ZM320-280h320v-280q0-66-47-113t-113-47q-66 0-113 47t-47 113v280Z"/></svg>
               </div>
               <p
                 className={`${
@@ -141,9 +141,9 @@ const SideLayout = () => {
               <div>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  height="24px"
+                  height="16px"
                   viewBox="0 -960 960 960"
-                  width="24px"
+                  width="16px"
                   className="fill-white"
                 >
                   <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
--- js/pages/Details.js
+++ js/pages/Details.js
@@ -1,10 +1,3 @@
-const { useState } = React;
-const { useQuery } = ReactQuery;
-
-const formatKey = (key) => {
-  return key.replace(/_/g, " ").replace(/\b\w/g, (char) => char.toUpperCase());
-};
-
 const Details = ({ path, id }) => {
   const [activeTab, setActiveTab] = useState("overview");
   const [active, setActive] = useState(null);
@@ -87,11 +80,13 @@ const Details = ({ path, id }) => {
   };
 
   const renderCellContent = (key, value) => {
+    
     if (isDate(value)) {
       return formatDate(value);
     }
     return String(value);
   };
+  
 
   const getHeaderCardProps = (path) => {
     if (path === "/jobs") {
@@ -117,36 +112,55 @@ const Details = ({ path, id }) => {
 
   const headerCardProps = getHeaderCardProps(path);
 
+  const tableHeaders =
+    path === "/jobs"
+      ? ["stage_name", "status", "start_date", "end_date"]
+      : Object.keys(item || {}).filter(
+          (key) =>
+            ![
+              "id",
+              "createdAt",
+              "archived",
+              "updatedAt",
+              "hs_lastmodifieddate",
+              "hs_createdate",
+              "hs_object_id",
+            ].includes(key)
+        );
+
+        const tableRows =
+        item && tableHeaders.length > 0 ? (
+          <TableBody>
+            <TableRow>
+              {tableHeaders.map((header) => (
+                <TableCell
+                  key={header}
+                  className="px-4 py-2 text-gray-900 dark:text-gray-100"
+                >
+                  {renderCellContent(header, item[header])}
+                </TableCell>
+              ))}
+              <TableCell>
+                <div className="bg-black w-fit p-1 rounded-md">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="20px"
+                    viewBox="0 -960 960 960"
+                    width="20px"
+                    fill="#EFEFEF"
+                  >
+                    <path d="M320-240h320v-80H320v80Zm0-160h320v-80H320v80ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z" />
+                  </svg>
+                </div>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        ) : null;
+      
+
   return (
     <div className="grid grid-cols-6 gap-4 h-full dark:bg-dark-200">
       <div className="col-span-4">
-        {/* <div className="border bg-gray-100 dark:bg-dark-300 rounded-lg w-fit p-1 border-flatGray">
-          <Tabs
-            activeTab={activeTab}
-            setActiveTab={setActiveTab}
-            className="rounded-md bg-white"
-          >
-            <TabsList className="">
-              <TabsTrigger
-                value="overview"
-                isActive={activeTab === "overview"}
-                onClick={() => handleTabClick("overview")}
-              >
-                Overview
-              </TabsTrigger>
-              <TabsTrigger
-                value="history"
-                isActive={activeTab === "history"}
-                onClick={() => handleTabClick("history")}
-              >
-                History
-              </TabsTrigger>
-            </TabsList>
-            <TabsContent value="overview"></TabsContent>
-            <TabsContent value="history"></TabsContent>
-          </Tabs>
-        </div> */}
-
         {isLoading && !item && <div className="loader-line"></div>}
 
         <HeaderCard
@@ -161,33 +175,52 @@ const Details = ({ path, id }) => {
 
         {(path === "/sites" || path === "/assets") && <MapsCard />}
 
-        <div>
-          <div>
-            <div className="p-3 dark:bg-dark-300 bg-white rounded-md mt-5 dark:text-white">
-              {item &&
-                filteredAndSortedEntries(item).map(
-                  ([key, value], index, array) => (
-                    <div
-                      key={key}
-                      className={`py-2 px-3 flex gap-x-5 ${
-                        index === array.length - 1 ? "" : ""
-                      }`}
-                    >
-                      <div className="text-sm font-semibold w-52">
-                        {formatKey(key)}:
-                      </div>
-                      <div className="text-sm text-gray-500">
-                        {renderCellContent(key, value)}
-                      </div>
-                    </div>
-                  )
+        {path === "/jobs" ? (
+          <div  className="col-span-4">
+            <Table className="w-full  my-8 bg-white rounded-md">
+              <TableHeader>
+                {tableHeaders.length > 0 && (
+                  <TableRow>
+                    {tableHeaders.map((header) => (
+                      <TableHead
+                        key={header}
+                        className="px-4 py-2 font-semibold text-xs text-gray-700 dark:text-gray-300 whitespace-nowrap"
+                      >
+                        {formatKey(header)}
+                      </TableHead>
+                    ))}
+
+                    <TableHead className="text-xs text-gray-700 dark:text-gray-300 whitespace-nowrap font-semibold">
+                      <span>Report</span>
+                    </TableHead>
+                  </TableRow>
                 )}
-            </div>
+              </TableHeader>
+              {tableRows}
+            </Table>
           </div>
-          {/* ) : (
-              <div className="dark:text-white">Under Construction</div>
-            )} */}
-        </div>
+        ) : (
+          <div className="p-3 dark:bg-dark-300 bg-white rounded-md mt-5 dark:text-white">
+            {item &&
+              filteredAndSortedEntries(item).map(
+                ([key, value], index, array) => (
+                  <div
+                    key={key}
+                    className={`py-2 px-3 flex gap-x-5 ${
+                      index === array.length - 1 ? "" : ""
+                    }`}
+                  >
+                    <div className="text-sm font-semibold w-52">
+                      {formatKey(key)}:
+                    </div>
+                    <div className="text-sm text-gray-500">
+                      {renderCellContent(key, value)}
+                    </div>
+                  </div>
+                )
+              )}
+          </div>
+        )}
       </div>
 
       <div className="col-span-2">
@@ -250,7 +283,7 @@ const Details = ({ path, id }) => {
                           {association.list.map((item) => (
                             <TableRow
                               key={item.id}
-                              className="border-b last:border-none"
+                              className="border-b whitespace-nowrap last:border-none"
                             >
                               {Object.entries(item)
                                 .filter(
