a6601ec516cef85364bcb122eac3325a8e3fed7c
Prosenjit Sarkar <prosenjit@digitalwoods.net>
2024-07-29 12:16:41 +0530

    Fix sidebar scroll

--- js/MainLayout.js
+++ js/MainLayout.js
@@ -6,7 +6,7 @@ const MainLayout = ({ children }) => {
     queryKey: ["features"],
     queryFn: async () => await Client.fetchFeatures.all,
     onSuccess: (response) => {
-      const allowedRoutes = [ "Asset", "Job", "Site" ];
+      const allowedRoutes = ["Asset", "Job", "Site"];
       const apiRoutes = response.data
         .filter((label) => allowedRoutes.includes(label.label))
         .map((label) => ({
@@ -19,7 +19,7 @@ const MainLayout = ({ children }) => {
   });
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div className="loader-line"></div>;
   }
 
   if (error) {
@@ -29,19 +29,18 @@ const MainLayout = ({ children }) => {
   const { Switch, Route } = ReactRouterDOM;
 
   return (
-    <div className="flex dark:bg-dark-200 bg-white ">
+    <div className="relative">
       <div
-        className={`lg:w-${
+        className={`transition-[width] duration-300 relative md:fixed min-h-screen w-full inset-0 md:w-${
           sidebarCollapsed ? "[100px]" : "[300px]"
-        } transition-[width] duration-300 max-h-[100vh]`}
+        }`}
       >
         <SideLayout />
       </div>
-
       <div
-        className={`lg:w-${
+        className={`dark:bg-dark-200 bg-flatGray transition-[width] duration-300 ml-auto w-full md:w-${
           sidebarCollapsed ? "[calc(100%_-_100px)]" : "[calc(100%_-_300px)]"
-        } w-[100%] dark:bg-dark-200 bg-flatGray transition-[width] duration-300 `}
+        }`}
       >
         {routes.length > 0 &&
           routes.map(({ path, title, icon }) => (
