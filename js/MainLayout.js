c875990c30e921f8284f928e0f22b6523f849adc
Prosenjit Sarkar <prosenjit@digitalwoods.net>
2024-07-30 11:22:53 +0530

    Fix dialog

--- js/MainLayout.js
+++ js/MainLayout.js
@@ -6,7 +6,7 @@ const MainLayout = ({ children }) => {
     queryKey: ["features"],
     queryFn: async () => await Client.fetchFeatures.all,
     onSuccess: (response) => {
-      const allowedRoutes = [ "Asset", "Job", "Site", "Equipment" ];
+      const allowedRoutes = ["Asset", "Job", "Site", "Equipment"];
       const apiRoutes = response.data
         .filter((label) => allowedRoutes.includes(label.label))
         .map((label) => ({
@@ -29,14 +29,12 @@ const MainLayout = ({ children }) => {
   const { Switch, Route } = ReactRouterDOM;
 
   return (
-    <div className="relative dark:bg-dark-200 bg-flatGray h-[100vh]">
-      <div
+    <div className="dark:bg-dark-200 bg-flatGray h-[100vh]">
+      <SideLayout
         className={`transition-[width] duration-300 relative md:fixed min-h-screen w-full inset-0 md:w-${
           sidebarCollapsed ? "[100px]" : "[300px]"
         }`}
-      >
-        <SideLayout />
-      </div>
+      />
       <div
         className={`dark:bg-dark-200 bg-flatGray transition-[width] duration-300 ml-auto w-full md:w-${
           sidebarCollapsed ? "[calc(100%_-_100px)]" : "[calc(100%_-_300px)]"
--- js/components/SiteLayout/SideLayout.js
+++ js/components/SiteLayout/SideLayout.js
@@ -11,7 +11,7 @@ const NavLink = ({ to, className, activeClassName, children }) => {
   );
 };
 
-const SideLayout = () => {
+const SideLayout = ({ className }) => {
   const [logoutDialog, setLogoutDialog] = useState(false);
   const { sidebarCollapsed, setSidebarCollapsed } = useCollapsible();
   const [isSecondIcon, setIsSecondIcon] = useState(false);
@@ -24,59 +24,106 @@ const SideLayout = () => {
   const { routes, setRoutes } = useRoute();
 
   return (
-    <div
-      className={`h-[100vh] overflow-hidden sticky top-0 px-6 pt-6 pb-8 transition-width bg-sidelayoutColor duration-300  dark:bg-dark-300 hidden lg:block`}
-    >
-      <div className="h-full flex flex-col">
-        <div className="flex justify-between items-center mb-8 h-[50px]">
-          <div className="flex items-center">
-            <div className="mr-2 w-8">
-              <Logo />
-            </div>
+    <div>
+      <div className={className}>
+        <div
+          className={`h-[100vh] overflow-hidden sticky top-0 px-6 pt-6 pb-8 transition-width bg-sidelayoutColor duration-300  dark:bg-dark-300 hidden lg:block`}
+        >
+          <div className="h-full flex flex-col">
+            <div className="flex justify-between items-center mb-8 h-[50px]">
+              <div className="flex items-center">
+                <div className="mr-2 w-8">
+                  <Logo />
+                </div>
 
-            <h1
-              className={`text-lg font-semibold pr-4 pl-1 text-white dark:text-white ${
-                sidebarCollapsed ? "hidden" : "block"
-              }`}
-            >
-              STONBURY
-            </h1>
-          </div>
-          <div
-            className="cursor-pointer flex items-center"
-            onClick={toggleSidebar}
-          >
-            {isSecondIcon ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="20px"
-                viewBox="0 -960 960 960"
-                width="20px"
-                className="fill-white"
+                <h1
+                  className={`text-lg font-semibold pr-4 pl-1 text-white dark:text-white ${
+                    sidebarCollapsed ? "hidden" : "block"
+                  }`}
+                >
+                  STONBURY
+                </h1>
+              </div>
+              <div
+                className="cursor-pointer flex items-center"
+                onClick={toggleSidebar}
               >
-                <path d="M440-240 200-480l240-240 56 56-183 184 183 184-56 56Zm264 0L464-480l240-240 56 56-183 184 183 184-56 56Z" />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="20px"
-                viewBox="0 -960 960 960"
-                width="20px"
-                className="fill-white"
-              >
-                <path d="M383-480 200-664l56-56 240 240-240 240-56-56 183-184Zm264 0L464-664l56-56 240 240-240 240-56-56 183-184Z" />
-              </svg>
-            )}
-          </div>
-        </div>
-        <nav className="space-y-1  flex-1">
-          <div className=" flex flex-col h-full justify-between ">
-            <div>
-              {routes.length > 0 &&
-                routes.map(({ path, title, icon }) => (
+                {isSecondIcon ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="20px"
+                    viewBox="0 -960 960 960"
+                    width="20px"
+                    className="fill-white"
+                  >
+                    <path d="M440-240 200-480l240-240 56 56-183 184 183 184-56 56Zm264 0L464-480l240-240 56 56-183 184 183 184-56 56Z" />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="20px"
+                    viewBox="0 -960 960 960"
+                    width="20px"
+                    className="fill-white"
+                  >
+                    <path d="M383-480 200-664l56-56 240 240-240 240-56-56 183-184Zm264 0L464-664l56-56 240 240-240 240-56-56 183-184Z" />
+                  </svg>
+                )}
+              </div>
+            </div>
+            <nav className="space-y-1  flex-1">
+              <div className=" flex flex-col h-full justify-between ">
+                <div>
+                  {routes.length > 0 &&
+                    routes.map(({ path, title, icon }) => (
+                      <NavLink
+                        key={path}
+                        to={path}
+                        className="block hover:bg-dark-400 dark:hover:bg-dark-400 dark:hover:text-white p-3 rounded-md no-underline"
+                        activeClassName="dark:bg-dark-600 dark:text-white bg-activeState"
+                      >
+                        <div
+                          className={`flex items-center gap-x-3 gap-y-1 ${
+                            sidebarCollapsed
+                              ? "justify-center"
+                              : "justify-start"
+                          }`}
+                        >
+                          <div>
+                            <SvgRenderer svgContent={icon} />
+                          </div>
+                          <p
+                            className={`${
+                              sidebarCollapsed
+                                ? "hidden opacity-0"
+                                : "opacity-100"
+                            } text-white dark:text-white text-sm font-medium transition-opacity duration-500 opacity-0 ml-2`}
+                          >
+                            {title}
+                          </p>
+                        </div>
+                      </NavLink>
+                    ))}
+                </div>
+
+                {!sidebarCollapsed && (
+                  <div>
+                    <div class="bg-custom-gradient text-white p-10 text-md text-center font-medium rounded-md">
+                      <p> Get the best Maintenance Service </p>
+                      <Button
+                        className="bg-white hover:bg-white text-blue-important mt-8"
+                        size="sm"
+                      >
+                        Go Now
+                      </Button>
+                    </div>
+                  </div>
+                )}
+
+                <div>
                   <NavLink
-                    key={path}
-                    to={path}
+                    key={"/notification"}
+                    to={"/notification"}
                     className="block hover:bg-dark-400 dark:hover:bg-dark-400 dark:hover:text-white p-3 rounded-md no-underline"
                     activeClassName="dark:bg-dark-600 dark:text-white bg-activeState"
                   >
@@ -86,129 +133,89 @@ const SideLayout = () => {
                       }`}
                     >
                       <div>
-                        <SvgRenderer svgContent={icon} />
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          height="20px"
+                          viewBox="0 -960 960 960"
+                          width="20px"
+                          className="fill-white"
+                        >
+                          <path d="M160-200v-80h80v-280q0-83 50-147.5T420-792v-28q0-25 17.5-42.5T480-880q25 0 42.5 17.5T540-820v28q80 20 130 84.5T720-560v280h80v80H160Zm320-300Zm0 420q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80ZM320-280h320v-280q0-66-47-113t-113-47q-66 0-113 47t-47 113v280Z" />
+                        </svg>
                       </div>
                       <p
                         className={`${
                           sidebarCollapsed ? "hidden opacity-0" : "opacity-100"
                         } text-white dark:text-white text-sm font-medium transition-opacity duration-500 opacity-0 ml-2`}
                       >
-                        {title}
+                        Notification Settings
                       </p>
                     </div>
                   </NavLink>
-                ))}
-            </div>
 
-            {!sidebarCollapsed && (
-              <div>
-                <div class="bg-custom-gradient text-white p-10 text-md text-center font-medium rounded-md">
-                  <p> Get the best Maintenance Service </p>
-                  <Button
-                    className="bg-white hover:bg-white text-blue-important mt-8"
-                    size="sm"
+                  <div
+                    className="block hover:bg-dark-400 dark:hover:bg-dark-400 dark:hover:text-white  p-3 rounded-md no-underline cursor-pointer"
+                    onClick={() => setLogoutDialog(true)}
                   >
-                    Go Now
-                  </Button>
+                    <div
+                      className={`flex items-center gap-x-3 gap-y-1 ${
+                        sidebarCollapsed ? "justify-center" : "justify-start"
+                      }`}
+                    >
+                      <div>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          height="20px"
+                          viewBox="0 -960 960 960"
+                          width="20px"
+                          className="fill-white"
+                        >
+                          <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
+                        </svg>
+                      </div>
+                      <p
+                        className={`${
+                          sidebarCollapsed ? "hidden" : ""
+                        } text-black dark:text-white text-sm font-medium text-white`}
+                      >
+                        Logout
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
-            )}
-
-            <div>
-              <NavLink
-                key={"/notification"}
-                to={"/notification"}
-                className="block hover:bg-dark-400 dark:hover:bg-dark-400 dark:hover:text-white p-3 rounded-md no-underline"
-                activeClassName="dark:bg-dark-600 dark:text-white bg-activeState"
-              >
-                <div
-                  className={`flex items-center gap-x-3 gap-y-1 ${
-                    sidebarCollapsed ? "justify-center" : "justify-start"
-                  }`}
-                >
-                  <div>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      height="20px"
-                      viewBox="0 -960 960 960"
-                      width="20px"
-                      className="fill-white"
-                    >
-                      <path d="M160-200v-80h80v-280q0-83 50-147.5T420-792v-28q0-25 17.5-42.5T480-880q25 0 42.5 17.5T540-820v28q80 20 130 84.5T720-560v280h80v80H160Zm320-300Zm0 420q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80ZM320-280h320v-280q0-66-47-113t-113-47q-66 0-113 47t-47 113v280Z" />
-                    </svg>
-                  </div>
-                  <p
-                    className={`${
-                      sidebarCollapsed ? "hidden opacity-0" : "opacity-100"
-                    } text-white dark:text-white text-sm font-medium transition-opacity duration-500 opacity-0 ml-2`}
-                  >
-                    Notification Settings
-                  </p>
-                </div>
-              </NavLink>
-
-              <div
-                className="block hover:bg-dark-400 dark:hover:bg-dark-400 dark:hover:text-white  p-3 rounded-md no-underline cursor-pointer"
-                onClick={() => setLogoutDialog(true)}
-              >
-                <div
-                  className={`flex items-center gap-x-3 gap-y-1 ${
-                    sidebarCollapsed ? "justify-center" : "justify-start"
-                  }`}
-                >
-                  <div>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      height="20px"
-                      viewBox="0 -960 960 960"
-                      width="20px"
-                      className="fill-white"
-                    >
-                      <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
-                    </svg>
-                  </div>
-                  <p
-                    className={`${
-                      sidebarCollapsed ? "hidden" : ""
-                    } text-black dark:text-white text-sm font-medium text-white`}
-                  >
-                    Logout
-                  </p>
-                </div>
-              </div>
-            </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+      <Dialog open={logoutDialog} onClose={setLogoutDialog}>
+        <div className=" bg-white dark:bg-dark-100 dark:text-white rounded-md flex-col justify-start items-center gap-6 inline-flex">
+          <div className="w-8">
+            <Logo />
           </div>
 
-          <Dialog open={logoutDialog} onClose={setLogoutDialog}>
-            <div className=" bg-white dark:bg-dark-100 dark:text-white rounded-md flex-col justify-start items-center gap-6 inline-flex">
-              <div className="w-8">
-                <Logo />
-              </div>
-
-              <div className="flex-col justify-start items-start gap-1 flex">
-                <div className="text-[#2F2E33] dark:text-white text-base font-semibold font-['Inter'] leading-snug">
-                  Log out of your account?
-                </div>
-              </div>
-              <div className="pt-3  sm:flex sm:flex-row-reverse gap-x-3">
-                <Button
-                  className="dark:text-white "
-                  onClick={() => setLogoutDialog(false)}
-                >
-                  Keep Me Logged In
-                </Button>
-                <Button
-                  variant="outline"
-                  className="dark:text-white"
-                  onClick={() => setLogoutDialog(false)}
-                >
-                  Logout
-                </Button>
-              </div>
+          <div className="flex-col justify-start items-start gap-1 flex">
+            <div className="text-[#2F2E33] dark:text-white text-base font-semibold font-['Inter'] leading-snug">
+              Log out of your account?
             </div>
-          </Dialog>
-        </nav>
-      </div>
+          </div>
+          <div className="pt-3  sm:flex sm:flex-row-reverse gap-x-3">
+            <Button
+              className="dark:text-white "
+              onClick={() => setLogoutDialog(false)}
+            >
+              Keep Me Logged In
+            </Button>
+            <Button
+              variant="outline"
+              className="dark:text-white"
+              onClick={() => setLogoutDialog(false)}
+            >
+              Logout
+            </Button>
+          </div>
+        </div>
+      </Dialog>
     </div>
   );
 };
