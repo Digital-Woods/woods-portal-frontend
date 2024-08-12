003726b1090a73c9996f465ef98fcbc09891cb75
Prosenjit Sarkar <prosenjit@digitalwoods.net>
2024-08-12 11:13:27 +0530

    add DetailsGallery

--- js/pages/Details.js
+++ js/pages/Details.js
@@ -65,37 +65,27 @@ const Details = ({ path, id }) => {
 
         {(path === "/sites" || path === "/assets") && <DetailsMapsCard />}
 
-        <div class="grid grid-cols-3 gap-4 pt-5">
-          {images.slice(0, 3).map((url, index) =>
-            index === 2 ? (
-              <div
-                key={index}
-                className={`relative items-center overflow-hidden bg-[url('${url}')] bg-no-repeat bg-center bg-cover cursor-pointer`}
-                onClick={() => setGalleryDialog(true)}
-              >
-                <div className="absolute inset-0 bg-black opacity-40"></div>
-                <div className="relative flex flex-col justify-center items-center px-4 text-white z-10 h-full">
-                  View More
-                </div>
-              </div>
-            ) : (
-              <img
-                key={index}
-                src={url}
-                alt={`Image ${index + 1}`}
-                className="w-full h-auto"
-              />
-            )
-          )}
-        </div>
-
         {path === "/jobs" ? (
           <div className="col-span-4">
             <DetailsTable item={item} path={path} />
+            {images.length > 0 && (
+              <DetailsGallery
+                images={images}
+                setGalleryDialog={setGalleryDialog}
+              />
+            )}
             <DetailsView sortItems={sortItems} />
           </div>
         ) : (
-          <DetailsView sortItems={sortItems} />
+          <div>
+            {images.length > 0 && (
+              <DetailsGallery
+                images={images}
+                setGalleryDialog={setGalleryDialog}
+              />
+            )}
+            <DetailsView sortItems={sortItems} />
+          </div>
         )}
       </div>
 
