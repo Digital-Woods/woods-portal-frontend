7e42d9d5d90422e617e1d85597d3dd5c10c51307
Ankan <ankanganguly24@gmail.com>
2024-09-04 11:50:49 +0530

    checked from isauthenticated

--- js/components/SiteLayout/PublicRoute.js
+++ js/components/SiteLayout/PublicRoute.js
@@ -1,13 +1,15 @@
 const { Switch, Route, Redirect } = ReactRouterDOM;
 
 const PublicRoute = ({ component: Component, restricted = true, ...rest }) => {
-  const token = getCookie(env.AUTH_TOKEN_KEY);
-
   return (
     <Route
       {...rest}
       render={(props) =>
-        token && restricted ? <Redirect to="/" /> : <Component {...props} />
+        isAuthenticated() && restricted ? (
+          <Redirect to="/" />
+        ) : (
+          <Component {...props} />
+        )
       }
     />
   );
