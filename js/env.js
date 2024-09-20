3d7381562bfb274c2edbf5bc00f1d861b92d80ef
Ankan <ankanganguly24@gmail.com>
2024-09-20 15:57:32 +0530

    Refactor env.js to update API_BASE_URL and remove ALLOWED_ROUTES

--- js/env.js
+++ js/env.js
@@ -1,9 +1,6 @@
 const env = {
-  // API_BASE_URL: "https://membership-portal-backend-demo-1.digitalwoods.io",
-  // API_BASE_URL: "https://dev1-marketplace.digitalwoods.io",
-  // API_BASE_URL: "https://dev2-membership-backend.digitalwoods.io",
+  // API_BASE_URL: "https://app.demo.digitalwoods.io",
   API_BASE_URL: "https://app.dev.one.digitalwoods.io",
-  ALLOWED_ROUTES: ["assets_new", "deals", "jobs", "sites", "assets"],
   AUTH_TOKEN_KEY: "testToken",
   AUTH_USER_KEY: "testUser",
   AUTH_PORTAL_KEY: "portalSettings",
