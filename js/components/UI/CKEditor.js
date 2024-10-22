f525b35a14a086f45d847e4fb072cbeddf0e726d
Prosenjit Sarkar <prosenjit@digitalwoods.net>
2024-10-22 18:14:02 +0530

    add Attachment

--- js/components/UI/CKEditor.js
+++ js/components/UI/CKEditor.js
@@ -58,7 +58,9 @@ function CustomUploadAdapterPlugin( editor ) {
 const CKEditor = ({ initialData = "", setEditorContent, id = 'new', api }) => {
     IMAGE_UPLOAD_URL = api
     const editorRef = useRef(id);
-    
+    const attachmentIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M720-330q0 104-73 177T470-80q-104 0-177-73t-73-177v-370q0-75 52.5-127.5T400-880q75 0 127.5 52.5T580-700v350q0 46-32 78t-78 32q-46 0-78-32t-32-78v-370h80v370q0 13 8.5 21.5T470-320q13 0 21.5-8.5T500-350v-350q-1-42-29.5-71T400-800q-42 0-71 29t-29 71v370q-1 71 49 120.5T470-160q70 0 119-49.5T640-330v-390h80v390Z"/></svg>`
+    const customLinkIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M720-330q0 104-73 177T470-80q-104 0-177-73t-73-177v-370q0-75 52.5-127.5T400-880q75 0 127.5 52.5T580-700v350q0 46-32 78t-78 32q-46 0-78-32t-32-78v-370h80v370q0 13 8.5 21.5T470-320q13 0 21.5-8.5T500-350v-350q-1-42-29.5-71T400-800q-42 0-71 29t-29 71v370q-1 71 49 120.5T470-160q70 0 119-49.5T640-330v-390h80v390Z"/></svg>`
+
     useEffect(() => {
         // Dynamically load CKEditor
         const loadCkEditor = () => {
@@ -70,59 +72,99 @@ const CKEditor = ({ initialData = "", setEditorContent, id = 'new', api }) => {
             };
 
             script.innerHTML = `
-                  import {
-                      ClassicEditor,
-                      Essentials,
-                      Paragraph,
-                      Bold,
-                      Italic,
-                      Underline,
-                      BlockQuote,
-                      List,
-                      Alignment,
-                      RemoveFormat,
-                      Link,
-                      Font,
-                      Image, 
-                      ImageToolbar, 
-                      ImageUpload,
-                      GeneralHtmlSupport
-                  } from 'ckeditor5';
+                    import {
+                        ClassicEditor,
+                        Essentials,
+                        Paragraph,
+                        Bold,
+                        Italic,
+                        Underline,
+                        BlockQuote,
+                        List,
+                        Alignment,
+                        RemoveFormat,
+                        Link,
+                        Font,
+                        Image, 
+                        ImageToolbar, 
+                        ImageUpload,
+                        GeneralHtmlSupport,
+                        Plugin,
+                        ButtonView
+                    } from 'ckeditor5';
+
+                    // Custom Attachment Plugin
+                    class AttachmentPlugin extends Plugin {
+                        init() {
+                            const editor = this.editor;
+
+                            // Add the attachment button to the editor
+                            editor.ui.componentFactory.add( 'attachment', locale => {
+                                const view = new ButtonView( locale );
+
+                                view.set( {
+                                    label: 'Attachment',
+                                    icon: '${attachmentIcon}',
+                                    tooltip: true
+                                } );
+
+                                // Handle the button click event
+                                view.on( 'execute', () => {
+                                    const input = document.createElement( 'input' );
+                                    input.type = 'file';
+                                    input.accept = '*/*';
+
+                                    input.onchange = () => {
+                                        const file = input.files[0];
+                                        if (file) {
+                                            alert(12121);
+                                        }
+                                    };
+
+                                    input.click();
+                                });
+
+                                return view;
+                            });
+
+                        }
+                    }
   
-                  ClassicEditor
-                      .create(document.querySelector('#${id}'), {
+                    ClassicEditor
+                        .create(document.querySelector('#${id}'), {
                         plugins: [
-                          Essentials, 
-                          Paragraph, 
-                          Bold, 
-                          Italic, 
-                          Underline, 
-                          BlockQuote, 
-                          List, 
-                          Alignment, 
-                          RemoveFormat, 
-                          Link, 
-                          Font,
-                          Image,
-                          ImageToolbar,
-                          ImageUpload,
-                          CustomUploadAdapterPlugin,
-                          GeneralHtmlSupport
+                            Essentials, 
+                            Paragraph, 
+                            Bold, 
+                            Italic, 
+                            Underline, 
+                            BlockQuote, 
+                            List, 
+                            Alignment, 
+                            RemoveFormat, 
+                            Link, 
+                            Font,
+                            Image,
+                            ImageToolbar,
+                            ImageUpload,
+                            CustomUploadAdapterPlugin,
+                            AttachmentPlugin,
+                            GeneralHtmlSupport
                         ],
                         toolbar: [
-                          "bold", "italic", "underline", "removeFormat", "|",
-                          {
+                            "bold", "italic", "underline", "removeFormat", "|",
+                            {
                             name: "moreFormatting",
                             items: [
-                              "fontFamily", "fontSize", "fontColor", "fontBackgroundColor",
-                              "blockquote", "bulletedList", "numberedList", "alignment"
+                                "fontFamily", "fontSize", "fontColor", "fontBackgroundColor",
+                                "blockquote", "bulletedList", "numberedList", "alignment"
                             ]
-                          },
-                          "|", 
-                          "link", "uploadImage", "|"
+                            },
+                            "|", 
+                            "link", "uploadImage", "attachment", "|"
                         ],
                         image: {
-                          toolbar: [ 'imageTextAlternative', 'toggleImageCaption', 'imageStyle:inline', 'imageStyle:block', 'imageStyle:side' ]
+                            toolbar: [ 'imageTextAlternative', 'toggleImageCaption', 'imageStyle:inline', 'imageStyle:block', 'imageStyle:side' ]
                         },
                         htmlSupport: {
                             allow: [
@@ -132,25 +174,25 @@ const CKEditor = ({ initialData = "", setEditorContent, id = 'new', api }) => {
                                 }
                             ]
                         }
-                      })
-                      .then(editor => {
-                          window.editor = editor;
-                          if (${initialData ? `'${initialData}'` : 'null'}) {
+                        })
+                        .then(editor => {
+                            window.editor = editor;
+                            if (${initialData ? `'${initialData}'` : 'null'}) {
                             editor.setData(${initialData ? `'${initialData}'` : 'null'});
-                          }
-                          editor.model.document.on("change:data", () => {
+                            }
+                            editor.model.document.on("change:data", () => {
                             const data = editor.getData();
                             window.onDataChange(data);
-                          });
-  
-                          // Move toolbar to the bottom
-                          const toolbarElement = editor.ui.view.toolbar.element;
-                          const editableElement = editor.ui.view.editable.element;
-                          editableElement.parentNode.insertBefore(toolbarElement, editableElement.nextSibling);
-                      })
-                      .catch(error => {
-                          console.error(error);
-                      });
+                            });
+
+                            // Move toolbar to the bottom
+                            const toolbarElement = editor.ui.view.toolbar.element;
+                            const editableElement = editor.ui.view.editable.element;
+                            editableElement.parentNode.insertBefore(toolbarElement, editableElement.nextSibling);
+                        })
+                        .catch(error => {
+                            console.error(error);
+                    });
               `;
             window.onDataChange = onDataChange;
             document.head.appendChild(script);
