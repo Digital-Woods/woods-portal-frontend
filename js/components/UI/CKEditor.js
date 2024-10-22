3e12952a07348e8feff92bd646f9104daeaa1446
Prosenjit Sarkar <prosenjit@digitalwoods.net>
2024-10-22 18:18:24 +0530

    clear code

--- js/components/UI/CKEditor.js
+++ js/components/UI/CKEditor.js
@@ -21,10 +21,10 @@ class CustomUploadAdapter {
               fetch(IMAGE_UPLOAD_URL, {
                 method: "POST",
                 headers: {
-                  "Content-Type": "application/json", // Send as JSON
+                  "Content-Type": "application/json",
                   Authorization: `Bearer ${token}`,
                 },
-                body: JSON.stringify(payload), // Convert payload to JSON string
+                body: JSON.stringify(payload),
               })
                 .then((response) => response.json())
                 .then((result) => {
@@ -39,30 +39,26 @@ class CustomUploadAdapter {
               reject(error);
             };
   
-            reader.readAsDataURL(file); // Convert file to Base64
+            reader.readAsDataURL(file);
           })
       );
     }
   
     abort() { }
-  }
+}
 
-// A custom plugin to register the custom upload adapter.
 function CustomUploadAdapterPlugin( editor ) {
     editor.plugins.get( 'FileRepository' ).createUploadAdapter = ( loader ) => {
         return new CustomUploadAdapter( loader );
     };
 }
 
-
 const CKEditor = ({ initialData = "", setEditorContent, id = 'new', api }) => {
     IMAGE_UPLOAD_URL = api
     const editorRef = useRef(id);
     const attachmentIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M720-330q0 104-73 177T470-80q-104 0-177-73t-73-177v-370q0-75 52.5-127.5T400-880q75 0 127.5 52.5T580-700v350q0 46-32 78t-78 32q-46 0-78-32t-32-78v-370h80v370q0 13 8.5 21.5T470-320q13 0 21.5-8.5T500-350v-350q-1-42-29.5-71T400-800q-42 0-71 29t-29 71v370q-1 71 49 120.5T470-160q70 0 119-49.5T640-330v-390h80v390Z"/></svg>`
-    const customLinkIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M720-330q0 104-73 177T470-80q-104 0-177-73t-73-177v-370q0-75 52.5-127.5T400-880q75 0 127.5 52.5T580-700v350q0 46-32 78t-78 32q-46 0-78-32t-32-78v-370h80v370q0 13 8.5 21.5T470-320q13 0 21.5-8.5T500-350v-350q-1-42-29.5-71T400-800q-42 0-71 29t-29 71v370q-1 71 49 120.5T470-160q70 0 119-49.5T640-330v-390h80v390Z"/></svg>`
 
     useEffect(() => {
-        // Dynamically load CKEditor
         const loadCkEditor = () => {
             const script = document.createElement('script');
             script.type = 'module';
@@ -198,7 +194,7 @@ const CKEditor = ({ initialData = "", setEditorContent, id = 'new', api }) => {
             document.head.appendChild(script);
         };
 
-        loadCkEditor(); // Invoke the dynamic loading of CKEditor when the component is mounted
+        loadCkEditor();
 
         return () => {
             if (window.editor) {
