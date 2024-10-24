be386e5508426f3c2e601f7e33317855d905baaf
Prosenjit Sarkar <prosenjit@digitalwoods.net>
2024-10-24 12:55:26 +0530

    Add Attachment

--- js/components/UI/Notes.js
+++ js/components/UI/Notes.js
@@ -2,7 +2,7 @@ const EditIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" /></svg>
 );
 
-const NoteCard = ({ note, objectId, id, api, refetch, setAlert }) => {
+const NoteCard = ({ note, objectId, id, imageUploadUrl, attachmentUploadUrl, refetch, setAlert }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [isOpenEditor, setIsOpenEditor] = useState(false);
   const [editorContent, setEditorContent] = useState("");
@@ -120,7 +120,7 @@ const NoteCard = ({ note, objectId, id, api, refetch, setAlert }) => {
             <div className="p-[24px] cursor-text"
               onClick={(e) => e.stopPropagation()}
             >
-              <CKEditor initialData={note.hs_note_body} setEditorContent={setEditorContent} id={`editor-${note.hs_object_id}`} api={api} />
+              <CKEditor initialData={note.hs_note_body} setEditorContent={setEditorContent} id={`editor-${note.hs_object_id}`} imageUploadUrl={imageUploadUrl} attachmentUploadUrl={attachmentUploadUrl} />
               <div className="flex gap-x-2 mt-2">
 
                 <Button
@@ -154,6 +154,7 @@ const Notes = ({ path, objectId, id }) => {
   const { me } = useMe();
   const [editorContent, setEditorContent] = useState("");
   const [imageUploadUrl, setImageUploadUrl] = useState("");
+  const [attachmentUploadUrl, setAttachmentUploadUrl] = useState("");
   const [page, setPage] = useState(1);
   const [alert, setAlert] = useState(null);
   const limit = 20;
@@ -206,6 +207,7 @@ const Notes = ({ path, objectId, id }) => {
   useEffect(() => {
     const portalId = getPortal().portalId
     setImageUploadUrl(`${env.API_BASE_URL}/api/${portalId}/hubspot-object-notes/images/${objectId}/${id}`)
+    setAttachmentUploadUrl(`${env.API_BASE_URL}/api/${portalId}/hubspot-object-notes/attachments/${objectId}/${id}/{noteId}`)
   }, []);
 
   if (isLoading) {
@@ -233,7 +235,7 @@ const Notes = ({ path, objectId, id }) => {
       </div>
       {results && results.rows.length > 0 ? (
         results.rows.map((note) => (
-          <NoteCard note={note} objectId={objectId} id={id} api={imageUploadUrl} refetch={refetch} setAlert={setAlert} />
+          <NoteCard note={note} objectId={objectId} id={id} imageUploadUrl={imageUploadUrl} attachmentUploadUrl={attachmentUploadUrl} refetch={refetch} setAlert={setAlert} />
         ))
       ) : (
         <div>No notes available.</div>
@@ -262,7 +264,7 @@ const Notes = ({ path, objectId, id }) => {
             {me.firstName}
           </p>
         </div>
-        <CKEditor setEditorContent={setEditorContent} api={imageUploadUrl} />
+        <CKEditor setEditorContent={setEditorContent} imageUploadUrl={imageUploadUrl} attachmentUploadUrl={attachmentUploadUrl} />
         <div className="mt-4 text-start">
           <Button
             disabled={isPosting || editorContent.trim() === ""}
